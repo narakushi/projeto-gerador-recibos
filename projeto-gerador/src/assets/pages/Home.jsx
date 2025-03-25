d44a459e44716dc5669429d60f9ea7b6679dcfff
narakushi <taina.santana1305@gmail.com>
2025-03-25 16:24:13 -0300

    :sparkles: feat: criando NavBar

--- projeto-gerador/src/assets/pages/Home.jsx
+++ projeto-gerador/src/assets/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "../ui/styles/Home.css";
+import NavBar from '../ui/components/Navbar';
 
 function Home() {
   return (
-    <div>Home</div>
+    <NavBar />
   )
 }
 
---
+++ projeto-gerador/src/assets/ui/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div><img src="" alt="" /></div>
+      <div>
+        <button>
+          Entrar
+        </button>
+        <button>
+          Fazer cadastro
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
