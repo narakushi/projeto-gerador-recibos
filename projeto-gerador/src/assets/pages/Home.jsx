aa4978bdf697ab97fe9776c56511167ae786aa56
narakushi <taina.santana1305@gmail.com>
2025-03-25 21:49:20 -0300

    :lipstick: feat: estilização da home

--- projeto-gerador/src/assets/pages/Home.jsx
+++ projeto-gerador/src/assets/pages/Home.jsx
@@ -3,10 +3,11 @@ import "../ui/styles/Home.css";
 import NavBar from "../ui/components/Navbar";
 import Title from "../ui/components/Title";
 import Form from "../ui/components/Form";
+import "../ui/styles/Home.css";
 
 function Home() {
   return (
-    <section>
+    <section className="home">
       <NavBar />
       <Title text="Bem-vindo ao gerador de recibos" />
       <p>Gere aqui de forma fácil seus documentos</p>
