21f88dbbcb61ddcafa3c4c5bc51e7e6da0822e5f
narakushi <taina.santana1305@gmail.com>
2025-03-26 20:48:32 -0300

    :lipstick: feat: estilizando inputs do formulario

--- projeto-gerador/src/assets/ui/components/Input.jsx
+++ projeto-gerador/src/assets/ui/components/Input.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/Input.css";
 
 const Input = ({ type, id, name, label }) => {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input type={type} name={name} id={id} />
+    <div className="container-input">
+      <label className="container-input__label" htmlFor={id}>{label}</label>
+      <input className="container-input__input" type={type} name={name} id={id} />
     </div>
   );
 };
