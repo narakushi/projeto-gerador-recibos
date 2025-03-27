642c570c1459834ac87c7ad3e9dfcb34dcfba23d
narakushi <taina.santana1305@gmail.com>
2025-03-27 15:59:47 -0300

    :recycle: refactor: reorganizando codigo home

--- projeto-gerador/src/App.jsx
+++ projeto-gerador/src/App.jsx
@@ -1,10 +1,13 @@
 import Router from "./Router";
 import Footer from "./assets/ui/components/Footer";
+import Container from "../src/assets/ui/components/Container";
 
 function App() {
   return (
     <>
-      <Router />
+      <Container>
+        <Router />
+      </Container>
       <Footer />
     </>
   );
--- projeto-gerador/src/assets/pages/Home.jsx
+++ projeto-gerador/src/assets/pages/Home.jsx
@@ -4,22 +4,21 @@ import NavBar from "../ui/components/Navbar";
 import Title from "../ui/components/Title";
 import Form from "../ui/components/Form";
 import "../ui/styles/Home.css";
-import inputsDataUser from "../ui/jsons/inputsDataUser.json"
-import Container from "../ui/components/Container";
+import inputsDataUser from "../ui/jsons/inputsDataUser.json";
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <Container>
+      <div className="home">
         <Title text="Bem-vindo ao gerador de recibos" />
         <p className="home__text">Gere aqui de forma fácil seus documentos</p>
         <div className="home__container-form">
-          <Form data={inputsDataUser}/>
+          <Form data={inputsDataUser} />
         </div>
-      </Container>
+      </div>
     </>
   );
-}
+};
 
 export default Home;
