8c035dbd9fc815dab09f98e85ff95a9f9602f320
narakushi <taina.santana1305@gmail.com>
2025-03-27 15:58:46 -0300

    :lipstick: feat: estilizando texto e imagem footer

--- projeto-gerador/src/assets/ui/components/Footer.jsx
+++ projeto-gerador/src/assets/ui/components/Footer.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import Button from "./Button";
 import logo from "../imgs/logo.svg";
+import "../styles/Footer.css"
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <img src={logo} alt="" />
-      </div>
-      <div></div>
-      <div>
-        <span>Todos os direitos reservados - Gerador de Recibos</span>
-        <span>Desenvolvido por Tainá Santana</span>
-      </div>
-      <div>
-        <Button text="Entrar" />
-        <Button text="Fazer cadastro" />
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__container-img">
+          <img src={logo} alt="" />
+          <div className="footer__pipe--right"></div>
+        </div>
+        <div className="footer__texts">
+          <span>Todos os direitos reservados - Gerador de Recibos</span>
+          <span>Desenvolvido por Tainá Santana</span>
+        </div>
+        <div className="footer__buttons">
+          <Button text="Entrar" customClass="button-variant" />
+          <div className="footer__pipe"></div>
+          <Button text="Fazer cadastro" />
+        </div>
       </div>
     </footer>
   );
