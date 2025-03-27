f2094ecaa9aa2d4190873a35e2bc645022219e72
narakushi <taina.santana1305@gmail.com>
2025-03-27 18:57:12 -0300

    :sparkles: feat: adicionando menu hamburguer

---
+++ projeto-gerador/src/assets/ui/components/HamburguerMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import "../styles/HamburguerMenu.css";
+
+const HamburguerMenu = () => {
+  return (
+    <>
+      <div className="menu-h">
+        <AiOutlineMenu />
+      </div>
+    </>
+  );
+};
+
+export default HamburguerMenu;
--- projeto-gerador/src/assets/ui/components/Navbar.jsx
+++ projeto-gerador/src/assets/ui/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { use } from "react";
 import logo from "../imgs/logo.svg";
 import Button from "../components/Button";
 import "../styles/NavBar.css";
 import { Link } from "react-router-dom";
+import HamburguerMenu from "./HamburguerMenu";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  
   return (
     <nav className="navbar">
       <div className="navbar__content">
@@ -16,6 +20,7 @@ const NavBar = () => {
           <div className="navbar__content__pipe"></div>
           <Button text="Fazer cadastro" />
         </div>
+        <HamburguerMenu />
       </div>
     </nav>
   );
