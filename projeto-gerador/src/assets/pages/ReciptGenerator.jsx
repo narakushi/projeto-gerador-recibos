1e856cecc6181a2dd0c056cdd5e2389e2dbb2ca4
narakushi <taina.santana1305@gmail.com>
2025-03-31 16:24:43 -0300

    :lipstick: feat: consertando css para footer ficar no fim da pagina

--- projeto-gerador/src/assets/pages/ReciptGenerator.jsx
+++ projeto-gerador/src/assets/pages/ReciptGenerator.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import NavBarIntern from '../ui/components/NavBarIntern';
 
 const ReciptGenerator = () => {
   return (
-    <div>ReciptGenerator</div>
+    <NavBarIntern />
   )
 }
 
